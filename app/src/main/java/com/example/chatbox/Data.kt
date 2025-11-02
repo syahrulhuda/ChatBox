73c6360940d9f60da1bb544e9aa057fe4f31637e
Syahrul Huda <syahrulhuda2910@gmail.com>
2025-11-02 14:53:24 +0700

    changes

--- app/src/main/java/com/example/chatbox/Data.kt
+++
@@ -1,7 +0,0 @@
-package com.example.chatbox
-
-data class User(val username: String = "", val email: String = "")
-
-data class UserItem(val uid: String = "", val username: String = "")
-
-data class Message(val senderId: String = "", val receiverId: String = "", val text: String = "", val timestamp: Long = 0)
